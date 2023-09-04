cc2592bc423e03c277f27f5fa5ef5af26b3a9ecb
Octavio Carrion <octcarbar@gmail.com>
2023-09-03 23:16:35 -0600

    Add repository objects

---
+++ Repository/IUsersRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entity;
+using Domain.Requests;
+using Domain.Response;
+
+namespace Repository
+{
+    public interface IUsersRepository
+    {
+        ICollection<User> GetAllUsers();
+
+        User GetUser(int UserId);
+
+        bool IsUniqueUser(string userName);
+
+        Task<LoginUserResponse> Login(LoginUserRequest loginUserDto);
+
+        Task<User> Register(CreateUserRequest createUserDto);
+    }
+}
